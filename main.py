affc6059442393041e4bae831f2b9ce3129d1c1c
ludago <luisgonzaleza.lol@gmail.com>
2024-09-19 16:59:46 -0600

    New changes on logic

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print ("Hello")
+import numpy as np
+
+empty_board = np.zeros((10,10))
+
+print (empty_board)
+
+
+def chooseCell (row: int, column: int, player_number: int) -> bool:
+
+
+def availablePlayer1 (row: int, column:int) -> bool:
+    if (row<=4 and column <=4):
+        return True
+    else:
+        return False
+
+    
+
